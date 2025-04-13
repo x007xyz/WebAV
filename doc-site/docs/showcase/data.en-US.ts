374eb0fda02431bce8c99c07ab01679088feb9e1
Rylan <rylanbot@qq.com>
2025-04-14 00:44:40 +0800

    feat: add a showcase page

---
+++ doc-site/docs/showcase/data.en-US.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: 'Melody Canvas',
+    cover:
+      'https://raw.githubusercontent.com/RylanBot/melody-canvas/refs/heads/main/public/image/preview.png',
+    description:
+      'An audio visualization editor that supports customizable effects and uses WebAV for video export.',
+    link: 'https://github.com/RylanBot/melody-canvas',
+  },
+];
---
+++ doc-site/docs/showcase/data.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: 'Melody Canvas',
+    cover:
+      'https://raw.githubusercontent.com/RylanBot/melody-canvas/refs/heads/main/public/image/preview.png',
+    description:
+      '音频可视化编辑器，支持自定义多种属性，采用 WebAV 导出视频效果。',
+    link: 'https://github.com/RylanBot/melody-canvas',
+  },
+];
