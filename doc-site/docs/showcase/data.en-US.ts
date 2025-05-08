0b7c7cd617a7b2a1725c256ac128b31369a2aead
hughfenghen <hughfenghen@gmail.com>
2025-05-08 17:14:05 +0800

    chore: update showcase

--- doc-site/docs/showcase/data.en-US.ts
+++ doc-site/docs/showcase/data.en-US.ts
@@ -2,6 +2,14 @@ export default {
   tips: 'Projects utilizing WebAV can submit their work via issues for recommendation—open-source projects are free; commercial products should contact the author to discuss sponsorship fees.',
   list: [
     {
+      name: 'WebAV Pro',
+      cover:
+        'https://github.com/WebAV-Tech/WebAV-Pro/blob/main/assets/preview.png?raw=true',
+      description:
+        'A commercial SDK extended from WebAV, helping users quickly build web-based audio and video products.',
+      link: 'https://github.com/WebAV-Tech/WebAV-Pro',
+    },
+    {
       name: 'Melody Canvas',
       cover:
         'https://raw.githubusercontent.com/RylanBot/melody-canvas/refs/heads/main/public/image/preview.png',
--- doc-site/docs/showcase/data.ts
+++ doc-site/docs/showcase/data.ts
@@ -2,6 +2,14 @@ export default {
   tips: '使用了 WebAV 的项目可创建 Issue 投稿推荐自己的作品，开源项目免费，商业产品请联系作者详聊赞助费。',
   list: [
     {
+      name: 'WebAV Pro',
+      cover:
+        'https://github.com/WebAV-Tech/WebAV-Pro/blob/main/assets/preview.png?raw=true',
+      description:
+        '基于 WebAV 扩展的商业版 SDK，帮助用户快速构建 Web 音视频产品。',
+      link: 'https://github.com/WebAV-Tech/WebAV-Pro',
+    },
+    {
       name: 'Melody Canvas',
       cover:
         'https://raw.githubusercontent.com/RylanBot/melody-canvas/refs/heads/main/public/image/preview.png',
